ea1715375450c9d2d7b23a166daf9d61a4628bb8
Pranav Vashi <neobuddy89@gmail.com>
2025-01-09 00:21:31 +0530

    SystemUI: Fix smartspace layout
    [aswin7469]: Updated for 16 QPR1
    
    Change-Id: If3a3bd1908c566b8a2d9195fbac75b6ecbdf9a9b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/SmartspaceSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/SmartspaceSection.kt
@@ -221,7 +221,7 @@ constructor(
                     connect(
                         sharedR.id.bc_smartspace_view,
                         ConstraintSet.TOP,
-                        sharedR.id.date_smartspace_view,
+                        ClockViewIds.LOCKSCREEN_CLOCK_VIEW_SMALL,
                         ConstraintSet.BOTTOM,
                     )
                 }
